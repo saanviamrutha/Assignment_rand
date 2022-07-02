4e5903df7ef3f37e9d5fad673bb52b3e23acbcbb
saanviamrutha <102648734+saanviamrutha@users.noreply.github.com>
2022-07-02 11:58:48 +0530

    Update exrand.c

--- codes/exrand.c
+++ codes/exrand.c
@@ -10,7 +10,7 @@ int  main(void) //main function begins
 uniform("uni.dat", 1000000);
 
 //Gaussian random numbers
-gaussian("gau.dat", 1000000);
+//gaussian("gau.dat", 1000000);
 
 //Mean of uniform
 //printf("%lf",mean("uni.dat"));
