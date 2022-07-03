8647e0ae47d420cab22b6a178af059816f39e064
saanviamrutha <102648734+saanviamrutha@users.noreply.github.com>
2022-07-03 13:37:15 +0530

    Add files via upload

---
+++ codes/1.4.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+double mean(char *str)
+{
+int i=0;
+FILE *fp;
+double x, temp=0.0;
+
+fp = fopen(str,"r");
+//get numbers from file
+while(fscanf(fp,"%lf",&x)!=EOF)
+{
+//Count numbers in file
+i=i+1;
+//Add all numbers in file
+temp = temp+x;
+}
+fclose(fp);
+temp = temp/(i-1);
+return temp;
+
+}
+//End function for calculating the mean of random numbers
+double var(char *str)
+{
+int i=0;
+FILE *fp;
+double x, temp=0.0,c;
+fp = fopen(str,"r");
+//get numbers from file
+c=mean(str);
+while(fscanf(fp,"%lf",&x)!=EOF)
+{
+//Count numbers in file
+i=i+1;
+//Add all numbers in file
+temp = temp+pow(x-c,2);
+}
+fclose(fp);
+temp = temp/(i-1);
+return temp;
+
+}
+//End function for calculating the mean of random numbers
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+//uniform("uni.dat", 1000000);
+
+//Gaussian random numbers
+//gaussian("gau.dat", 1000000);
+
+//Mean of uniform
+printf("%lf \n",mean("uni.dat"));
+//Variance of uniform
+printf("%lf \n",var("uni.dat"));
+return 0;
+}
